e687bf9c12a48e33de6e812ce45c5ab7a7cfcd47
Maciej Sobkowski <maciejjo@maciejjo.pl>
2016-06-01 17:33:46 +0200

    Add quotes to build.sh to prevent splitting

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-MYPATH="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
+MYPATH=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 
-. ${MYPATH}/config.sh
+. "${MYPATH}/config.sh"
 
-if [ ! -e ${TARGET_DIR} ] ; then
+if [ ! -e "${TARGET_DIR}" ] ; then
 	echo "Directory \"${TARGET_DIR}\" dir not found! Please run setup.sh script first."
 	exit 1
 fi
 
 docker run \
 	--net=host --rm -it \
-	-v $(pwd):$(pwd)    \
-	-v ${YOCTO_DIR}:${YOCTO_DIR} \
-	-v ~/.ssh:/home/${USER}/.ssh \
-	-v ~/.gitconfig:/home/${USER}/.gitconfig \
-	${DOCKER_IMAGE}  /bin/bash \
+	-v "$(pwd)":"$(pwd)"    \
+	-v "${YOCTO_DIR}":"${YOCTO_DIR}" \
+	-v "${HOME}/.ssh:/home/${USER}/.ssh" \
+	-v "${HOME}/.gitconfig:/home/${USER}/.gitconfig" \
+	"${DOCKER_IMAGE}"  "/bin/bash" \
 	-c "cd $(pwd) && source oe-core/oe-init-build-env ${TARGET_DIR} ${BITBAKE_DIR} \
 	&& bitbake ${IMAGE_NAME}"
