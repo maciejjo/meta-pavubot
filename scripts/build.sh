fa518c0d3bf748c20ecd059d9924af399d102288
Maciej Sobkowski <maciejjo@maciejjo.pl>
2016-07-18 18:38:41 +0200

    Update building scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,21 @@ MYPATH=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 
 . "${MYPATH}/config.sh"
 
+
+if [ ! $# -eq 1 ]; then
+	echo "Usage: $0 [image|sdk]"
+	exit 1
+fi
+
+if [ $1 == "image" ]; then
+	BUILD_TARGET=${IMAGE_NAME}
+elif [ $1 == "sdk" ]; then
+	BUILD_TARGET="meta-toolchain"
+else
+	echo "Usage: $0 [image|sdk]"
+	exit 1
+fi
+
 if [ ! -e "${TARGET_DIR}" ] ; then
 	echo "Directory \"${TARGET_DIR}\" dir not found! Please run setup.sh script first."
 	exit 1
@@ -17,4 +32,4 @@ docker run \
 	-v "${HOME}/.gitconfig:/home/${USER}/.gitconfig" \
 	"${DOCKER_IMAGE}"  "/bin/bash" \
 	-c "cd $(pwd) && source oe-core/oe-init-build-env ${TARGET_DIR} ${BITBAKE_DIR} \
-	&& bitbake ${IMAGE_NAME}"
+	&& bitbake ${BUILD_TARGET}"
--- scripts/config.sh
+++ scripts/config.sh
@@ -2,7 +2,17 @@ MACHINE="beaglebone"
 DISTRO="poky-systemd"
 
 YOCTO_DIR="/usr/local/share/yocto"
-DOCKER_IMAGE="cbrake/oe-build"
+DOCKER_IMAGE="oe-build:custom"
 BITBAKE_DIR="bitbake"
 TARGET_DIR=${MACHINE}
 IMAGE_NAME="pavubot-image"
+
+NFS_ROOT="/srv/bbb_nfs"
+TFTP_ROOT="/srv/bbb_tftp"
+
+DEPLOY_PATH="build-glibc/deploy/images/beaglebone"
+ZIMAGE_FILE="zImage"
+DTB_FILE="zImage-am335x-boneblack.dtb"
+ZIMAGE_DEST_FILE="zImage"
+DTB_DEST_FILE="am335x-boneblack.dtb"
+ROOTFS_FILE="${IMAGE_NAME}-${MACHINE}.tar.gz"
